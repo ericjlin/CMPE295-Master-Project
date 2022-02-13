be11d1bb74f2c10372c2332e183c60af567dcb5b
sunnyD777 <skohli200@gmail.com>
2022-02-13 13:37:00 -0800

    added styling to login and registration

--- water-hardness-system/navigation/index.tsx
+++ water-hardness-system/navigation/index.tsx
@@ -27,7 +27,8 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+      >
       {user ? 
        <RootNavigator /> :
        <AuthScreen />
